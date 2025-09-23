fa113c7a5c8d5fddb4668bd8392d3bf0fcd56d7a
Cynthia Tristán <cyntrist@ucm.es>
2025-09-23 10:44:30 +0200

    auuu 2-3

--- 2-1/2-1.cpp
+++ 2-1/2-1.cpp
@@ -1,19 +1,95 @@
-// 2-1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
+/*@ <authors>
+ *
+ * Cynthia Tristán Álvarez
+ * MARP62
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
 
-int main()
+/*@ <answer>
+
+
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+using llong = long long;
+using uint = unsigned int;
+
+llong resuelve(priority_queue<llong, vector<llong>, greater<llong>>& cola)
 {
-    std::cout << "Hello World!\n";
+	if (cola.empty() || cola.size() == 1)
+		return 0;
+
+	llong res;
+	res = 0;
+
+	while (cola.size() >= 2)
+	{
+		llong a = cola.top(); cola.pop();
+		llong b = cola.top(); cola.pop();
+		llong suma = a + b;
+		res += suma;
+
+		cola.push(suma); // !!!!!!!!!
+	}
+
+	//if (cola.size() == 1)
+	//	res += cola.top(); cola.pop();
+
+	return res;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+// https://learn.microsoft.com/en-us/cpp/cpp/data-type-ranges?view=msvc-170
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	uint N;
+	cin >> N;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	if (N == 0)
+		return false;
+
+	priority_queue<llong, vector<llong>, greater<llong>> cola; // queremos una cola por mínimos para sumar los menores seguidos (top y pop)
+	for (uint i = 0; i < N; i++)
+	{
+		llong n;
+		cin >> n;
+		cola.push(n); 
+	}
+
+	cout << resuelve(cola) << '\n';
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
