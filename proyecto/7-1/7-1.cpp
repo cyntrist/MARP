2bfa634381607e4e2195704f10a958fd301abf27
Cynthia Tristán <cyntrist@ucm.es>
2025-10-30 01:17:10 +0100

    XD hola

--- proyecto/7-1/7-1.cpp
+++ proyecto/7-1/7-1.cpp
@@ -1,19 +1,123 @@
-// 7-1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+/*@ <authors>
+ *
+ * Cynthia Tristán Álvarez
+ * MARP62
+ *
+ *@ </authors> */
+
+#include <deque>
 #include <iostream>
+#include <fstream>
+#include "DigrafoValorado.h"
+#include "IndexPQ.h"
+using namespace std;
 
-int main()
+/*@ <answer>
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo (después de la marca)
+ //@ <answer>
+
+template <typename Valor>
+using Camino = deque<Valor>; // para representar caminos
+template <typename Valor>
+class Dijkstra
 {
-    std::cout << "Hello World!\n";
+public:
+    Dijkstra(DigrafoValorado<Valor> const& g, int orig) : origen(orig),
+        dist(g.V(), INF), ulti(g.V()), pq(g.V())
+    {
+        dist[origen] = 0;
+        pq.push(origen, 0);
+        while (!pq.empty())
+        {
+            int v = pq.top().elem; pq.pop();
+            for (auto a : g.ady(v))
+                relajar(a);
+        }
+    }
+
+    bool hayCamino(int v) const { return dist[v] != INF; }
+
+    Valor distancia(int v) const { return dist[v]; }
+    Camino<Valor> camino(int v) const
+	{
+        Camino<Valor> cam;
+        // recuperamos el camino retrocediendo
+        AristaDirigida<Valor> a;
+        for (a = ulti[v]; a.desde() != origen; a = ulti[a.desde()])
+            cam.push_front(a);
+        cam.push_front(a);
+        return cam;
+    }
+
+private:
+    const Valor INF = std::numeric_limits<Valor>::max();
+    int origen;
+    vector<Valor> dist;
+    vector<AristaDirigida<Valor>> ulti;
+    IndexPQ<int, Valor> pq;
+    void relajar(AristaDirigida<Valor> a)
+	{
+        int v = a.desde(), w = a.hasta();
+        if (dist[w] > dist[v] + a.valor()) 
+        {
+            dist[w] = dist[v] + a.valor(); ulti[w] = a;
+            pq.update(w, dist[w]);
+        }
+    }
+};
+
+bool resuelveCaso()
+{
+    int N, C;
+    cin >> N >> C;
+    if (!cin)
+        return false;
+
+    DigrafoValorado<int> dg(N);
+    for (int i = 0; i < C; i++)
+    {
+        int v, w, d;
+        cin >> v >> w >> d;
+        dg.ponArista(AristaDirigida<int>(v - 1, w - 1, d));
+    }
+
+    int P;
+    cin >> P;
+
+    for (int i = 0; i < P; i++)
+    {
+        int v, w;
+        cin >> v >> w;
+    }
+
+    return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba debajo de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    if (!in.is_open())
+        cout << "Error: no se ha podido abrir el archivo de entrada." << endl;
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    cout << "Pulsa Intro para salir..." << flush;
+    cin.get();
+#endif
+    return 0;
+}
--- proyecto/7-3/7-3.cpp
+++ proyecto/7-3/7-3.cpp
@@ -1,19 +1,106 @@
-// 7-3.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
+/*@ <authors>
+ *
+ * Cynthia Tristán Álvarez
+ * MARP62
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include "DigrafoValorado.h"
+using namespace std;
 
-int main()
+/*@ <answer>
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo (después de la marca)
+ //@ <answer>
+
+class CaminosMin
 {
-    std::cout << "Hello World!\n";
+public:
+    CaminosMin(DigrafoValorado<int> const& g, int orig) :
+        origen(orig), dist(g.V(), INF), ulti(g.V()), pq(g.V()), caminos(g.V(), 0) {
+        dist[origen] = 0;
+        caminos[origen] = 1;
+        pq.push(origen, 0);
+        while (!pq.empty()) {
+            int v = pq.top().elem; pq.pop();
+            for (auto a : g.ady(v))
+                relajar(a);
+        }
+    }
+
+    int64_t calculaNCaminos(DigrafoValorado<int> const& g, int v) {
+        if (!hayCamino(v)) return 0;
+
+        if (caminos[v] == 0) {
+            int64_t min = distancia(v);
+            for (auto a : g.ady(v)) {
+                int w = a.hasta();
+                if (dist[w] + a.valor() <= min) {
+                    caminos[v] += calculaNCaminos(g, w);
+                }
+            }
+        }
+        return caminos[v];
+    }
+
+    bool hayCamino(int v) const { return dist[v] != INF; }
+    int64_t distancia(int v) const { return dist[v]; }
+    int64_t nCaminos(int v) const { return caminos[v]; }
+
+private:
+    const int INF = 1000000000;
+    int origen;
+    std::vector<int64_t> dist;
+    std::vector<int64_t> caminos; //Caminos minimos a cada indice
+    std::vector<AristaDirigida<int>> ulti;
+    IndexPQ<int64_t> pq;
+
+    void relajar(AristaDirigida<int> a) {
+        int v = a.desde(), w = a.hasta();
+        if (dist[w] > dist[v] + a.valor()) {
+            dist[w] = dist[v] + a.valor(); ulti[w] = a;
+            pq.update(w, dist[w]);
+        }
+    }
+};
+
+
+bool resuelveCaso()
+{
+    int N, M, A;
+    cin >> N >> M >> A;
+    if (!cin)
+        return false;
+
+    return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba debajo de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+    // ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+    ifstream in("casos.txt");
+    if (!in.is_open())
+        cout << "Error: no se ha podido abrir el archivo de entrada." << endl;
+    auto cinbuf = cin.rdbuf(in.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+    // para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+    cin.rdbuf(cinbuf);
+    cout << "Pulsa Intro para salir..." << flush;
+    cin.get();
+#endif
+    return 0;
+}
