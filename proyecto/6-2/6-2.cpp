0a57a7eac4101f9845525aea96cf7d595606fd2c
Cynthia Tristán <cyntrist@ucm.es>
2026-01-04 03:32:08 +0100

    6-2

--- proyecto/6-2/6-2.cpp
+++ proyecto/6-2/6-2.cpp
@@ -1,19 +1,124 @@
-// 6-2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <deque>
 #include <iostream>
+#include <fstream>
+#include "GrafoValorado.h"
 
-int main()
+using namespace std;
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+class Trayecto
 {
-    std::cout << "Hello World!\n";
+public:
+	Trayecto(GrafoValorado<int> const& g, int origen, int destino, int umbral) : visit(g.V(), false),
+		apilado(g.V(), false),
+		_posible(false),
+		_ciclico(false)
+	{
+		dfs(g, origen, destino, umbral);
+	}
+
+	bool ciclico() const { return _ciclico; }
+	bool posible() const { return _posible;  }
+
+private:
+	vector<bool> visit;
+	vector<bool> apilado;
+	bool _ciclico;
+	bool _posible;
+
+	void dfs(GrafoValorado<int> const& g, int v, int destino, int umbral)
+	{
+		visit[v] = true;
+		apilado[v] = true;
+		if (v == destino)
+			_posible = true;
+		else
+		{
+			for (Arista<int> a : g.ady(v))
+			{
+				if (a.valor() < umbral) 
+					continue;
+
+				int w = a.otro(v);
+				if (!visit[w]) 
+					dfs(g, w, destino, umbral);
+				else if (apilado[w]) 
+					_ciclico = true;
+			}
+		}
+		apilado[v] = false;
+	}
+};
+
+bool resuelveCaso() {
+	int V, E;
+	cin >> V >> E;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	GrafoValorado<int> grafo(V);
+	int u, v, valor;
+	for (int i = 0; i < E; ++i)
+	{
+		cin >> u >> v >> valor;
+		grafo.ponArista({ u - 1,v - 1, valor });
+	}
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int O, D, U;
+		cin >> O >> D >> U;
+		Trayecto t(grafo, O - 1, D - 1, U);
+		cout << (t.posible() ? "SI" : "NO") << '\n';
+	}
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
