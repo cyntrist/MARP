b4f7fb0dca1a8f136f0df6fb1a4f4bcbfc626799
Cynthia Tristán <cyntrist@ucm.es>
2026-01-13 21:49:28 +0100

    r-17

--- proyecto/R-17/R-17.cpp
+++ proyecto/R-17/R-17.cpp
@@ -1,19 +1,79 @@
-// R-17.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// R-17.cpp : Este archivo contiene la función "main". La ejecución del programa 
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <queue>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include "PriorityQueue.h"
+using namespace std;
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool resuelveCaso() {
+	int N;
+	cin >> N;
+
+	if (!std::cin)
+		return false;
+
+	priority_queue<int> pq;
+	for (int i = 0; i < N; ++i)
+	{
+		int p;
+		cin >> p;
+		pq.push(p);
+	}
+
+	int resultado = 0;
+	int n = pq.size();
+	while (pq.size() >= 3)
+	{
+		pq.pop();
+		pq.pop();
+		int c = pq.top(); pq.pop();
+		resultado += c;
+		n -= 3;
+	}
+	cout << resultado << '\n';
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
