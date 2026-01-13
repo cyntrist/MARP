24896baf230b21f819863bc7fe7d6a507e3e2852
Cynthia Tristán <cyntrist@ucm.es>
2026-01-13 04:14:39 +0100

    r-2

--- proyecto/R-02/R-02.cpp
+++ proyecto/R-02/R-02.cpp
@@ -1,19 +1,110 @@
-// R-02.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <deque>
 #include <iostream>
+#include <fstream>
+#include "Grafo.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+class Bipartito {
+private:
+	vector<bool> visit; // visit[v] = ¿hay camino de s a v?
+	vector<int> ant; // ant[v] = último vértice antes de llegar a v
+
+	vector<bool> equipos;
+	bool _bipartito;
+	void dfs(Grafo const& G, int v) {
+		visit[v] = true;
+		for (int w : G.ady(v)) {
+			if (!_bipartito) return;
+
+			if (!visit[w]) {
+				ant[w] = v;
+				equipos[w] = !equipos[v];
+				dfs(G, w);
+			}
+			else if (equipos[w] == equipos[v])
+			{
+				_bipartito = false; return;
+			}
+		}
+	}
+public:
+	Bipartito(Grafo const& g) : visit(g.V(), false), _bipartito(true),
+		ant(g.V()), equipos(g.V(), false) {
+		for (int v = 0; v < g.V(); ++v)
+		{
+			if (!visit[v])
+			{
+				dfs(g, v);
+				if (!_bipartito) break;
+			}
+		}
+	}
+	bool es() const { return _bipartito; }
+};
+
+bool resuelveCaso() {
+	int N, M;
+	cin >> N >> M;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+	Grafo g(N);
+	while (M--)
+	{
+		int v, w;
+		cin >> v >> w;
+		--v; --w;
+		g.ponArista(v, w);
+	}
+
+	Bipartito b(g);
+	cout << (b.es() ? "SI" : "NO") << '\n'; 
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
