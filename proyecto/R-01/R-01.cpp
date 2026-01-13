3b8bc57642bce56bc364298748c0614718b56425
Cynthia Tristán <cyntrist@ucm.es>
2026-01-13 03:45:31 +0100

    r-1

--- proyecto/R-01/R-01.cpp
+++ proyecto/R-01/R-01.cpp
@@ -1,19 +1,158 @@
-// R-01.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <deque>
 #include <iostream>
+#include <fstream>
+#include <limits>
+#include "GrafoValorado.h"
+#include "ConjuntosDisjuntos.h"
+#include "PriorityQueue.h"
+#include "IndexPQ.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+template <typename Valor>
+class Dijkstra {
+public:
+	Dijkstra(GrafoValorado<Valor> const& g, int orig) : origen(orig),
+		_max(g.V(), INF), ulti(g.V()), pq(g.V()) {
+		_max[origen] = 0;
+		pq.push(origen, 0);
+		while (!pq.empty()) {
+			int v = pq.top().elem; pq.pop();
+			for (auto a : g.ady(v))
+				relajar(a, v);
+		}
+	}
+	bool hayCamino(int v) const { return _max[v] != INF; }
+	Valor maximo(int v) const { return _max[v]; }
+private:
+	const Valor INF = std::numeric_limits<Valor>::max();
+	int origen;
+	std::vector<Valor> _max;
+	std::vector<Arista<Valor>> ulti;
+	IndexPQ<Valor> pq;
+
+	void relajar(Arista<Valor> a, int v) {
+		int w = a.otro(v);
+		Valor nuevo = max(_max[v], a.valor());
+		if (nuevo < _max[w]) {
+			_max[w] = nuevo;
+			pq.update(w, _max[w]);
+		}
+	}
+};
+
+
+
+template <typename Valor>
+class ARM_Kruskal {
+	ConjuntosDisjuntos cjtos;
+	Valor _maximo;
+public:
+	Valor maximo() const { return _maximo;  }
+	bool unidos(int v, int w) const { return cjtos.unidos(v, w); }
+	ARM_Kruskal(GrafoValorado<Valor> const& g, int P, int T) : cjtos(g.V()), _maximo(0) {
+		if (P == T) return; 
+		PriorityQueue<Arista<Valor>> pq(g.aristas());
+		while (!pq.empty()) {
+			auto a = pq.top(); pq.pop();
+			int v = a.uno(), w = a.otro(v);
+			if (!cjtos.unidos(v, w)) {
+				cjtos.unir(v, w);
+				if (cjtos.unidos(P, T))
+				{
+					_maximo = a.valor(); // por definicion de kruskal voraz, si es la primera en unirles es la de mayor valor posible
+					break;
+				}
+			}
+		}
+	}
+};
+
+bool resuelveCaso() {
+	int N, M;
+	cin >> N >> M;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	GrafoValorado<int> g(N);
+	while (M--)
+	{
+		int v, w, p;
+		cin >> v >> w >> p;
+		--v; --w;
+		g.ponArista({v, w, p});
+	}
+
+	int P, T;
+	cin >> P >> T;
+	--P; --T;
+
+
+	ARM_Kruskal k(g, P, T);
+	if (k.unidos(P, T))
+	{
+		cout << k.maximo() << '\n';
+	}
+	else
+	{
+		cout << "IMPOSIBLE\n";
+	}
+	//Dijkstra d(g, P);
+	//if (d.hayCamino(T))
+	//{
+	//	cout << d.maximo(T) << '\n';
+	//}
+	//else
+	//{
+	//	cout << "IMPOSIBLE\n";
+	//}
+	//cout << "---\n";
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
