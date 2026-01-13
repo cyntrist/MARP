6fbdd393086c029b14b2d01c4363353b9318057d
Cynthia Tristán <cyntrist@ucm.es>
2026-01-13 18:53:30 +0100

    r-4 y r-6

--- proyecto/R-04/R-04.cpp
+++ proyecto/R-04/R-04.cpp
@@ -1,19 +1,101 @@
-// R-04.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+#include "GrafoValorado.h"  // propios o los de las estructuras de datos de clase
+#include "ConjuntosDisjuntos.h"  // propios o los de las estructuras de datos de clase
+#include "PriorityQueue.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+template <typename Valor>
+class ARM_Kruskal {
+private:
+	Valor _maximo;
+	ConjuntosDisjuntos cjtos;
+public:
+	Valor maximo() const { return _maximo; }
+	bool conexo() const { return cjtos.num_cjtos() == 1; }
+	ARM_Kruskal(GrafoValorado<Valor> const& g) : _maximo(0), cjtos(g.V()) {
+		PriorityQueue<Arista<Valor>> pq(g.aristas());
+		while (!pq.empty()) {
+			auto a = pq.top(); pq.pop();
+			int v = a.uno(), w = a.otro(v);
+			if (!cjtos.unidos(v, w)) {
+				cjtos.unir(v, w);
+				_maximo = max(_maximo, a.valor());
+			}
+		}
+	}
+};
+
+void resuelveCaso() {
+	int N, M;
+	cin >> N >> M;
+
+	GrafoValorado<int> g(N);
+	while (M--)
+	{
+		int v, w, p;
+		cin >> v >> w >> p;
+		--v; --w;
+		g.ponArista({ v, w, p });
+	}
+
+	ARM_Kruskal k(g);
+	if (k.conexo())
+	{
+		cout << k.maximo() << '\n'; 
+	}
+	else
+	{
+		cout << "Imposible\n";
+	}
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+	// para dejar todo como estaba al principio y parar antes de salir
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+
+	return 0;
+}
--- proyecto/R-06/R-06.cpp
+++ proyecto/R-06/R-06.cpp
@@ -1,19 +1,110 @@
-// R-06.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+#include "GrafoValorado.h"
+#include "PriorityQueue.h"
+#include "ConjuntosDisjuntos.h"
+#include "IndexPQ.h"
+#include "Grafo.h"
+#include "DigrafoValorado.h"
+
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+class MaximaCompConexa {
+public:
+	MaximaCompConexa(Grafo const& g, vector<int> const& sobornos) : visit(g.V(), false), minim(0) {
+		for (int v = 0; v < g.V(); ++v) {
+			if (!visit[v]) { // se recorre una nueva componente conexa
+				int minimoComponente = dfs(g, v, sobornos);
+				minim += minimoComponente;
+			}
+		}
+	}
+
+	int minimo() const { return minim; }
+private:
+	vector<bool> visit; // visit[v] = se ha visitado el vértice v?
+	int minim; 
+
+	int dfs(Grafo const& g, int v, vector<int> const& sobornos) {
+		visit[v] = true;
+		int _maxAct = sobornos[v];
+		for (int w : g.ady(v)) {
+			if (!visit[w])
+				_maxAct = min(_maxAct, dfs(g, w, sobornos));
+		}
+		return _maxAct;
+	}
+};
+
+bool resuelveCaso() {
+	int N, M;
+	cin >> N >> M;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	vector<int> sobornos(N);
+	for (int i = 0; i < N; ++i)
+		cin >> sobornos[i];
+
+	Grafo g(N);
+	while (M--)
+	{
+		int v, w;
+		cin >> v >> w;
+		--v; --w;
+		g.ponArista(v, w);
+	}
+
+	MaximaCompConexa m(g, sobornos);
+	cout << m.minimo() << '\n';
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
