78ca8965a36e17f84231f2296bc82aab87908df0
Cynthia Tristán <cyntrist@ucm.es>
2026-06-18 04:00:49 +0200

    enero programcvion dinamica
    ni siquiera habia que hacerlo con matriz...

--- proyecto/EN-2/EN-2.cpp
+++ proyecto/EN-2/EN-2.cpp
@@ -1,20 +1,120 @@
-// EN-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main()
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+void beneficio(int N, int K, 
+	std::vector<int>& m, const std::vector<int>& beneficios, 
+	const std::vector<int>& prev)
 {
-    std::cout << "Hello World!\n";
+	for (int i = 1; i <= N; ++i)
+	{
+		int ir = i - 1; // indice real del area
+		int no_coger = m[i - 1]; // beneficio anterior
+		int coger = beneficios[ir] + m[prev[ir] + 1]; // beneficio de este area
+		m[i] = std::max(coger, no_coger);
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N = 0, K = 0;
+	std::cin >> N >> K;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	// resolver el caso posiblemente llamando a otras funciones
+	std::vector<int> areas(N);
+	for (auto& k : areas)
+		std::cin >> k;
+
+	std::vector<int> beneficios(N);
+	for (auto& b : beneficios)
+		std::cin >> b;
+
+	std::vector<int> m(N + 1, 0); // maximo beneficio obtenible hasta el area i
+	std::vector<int> prev(N, -1); // para el área de índice i, prev[i] = índice j < i del área más cercana compatible con la restricción de distancia, es decir: pos[i] - pos[j] >= K
+
+	// calculo del area previa
+	int j = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		while (j < i && areas[i] - areas[j] >= K) ++j;
+		prev[i] = j - 1;
+	}
+
+	// prog dinam
+	beneficio(N, K, m, beneficios, prev);
+
+	// escribir la solución
+	std::cout << m[N] << '\n';
+	// reconstrucción
+	int i = N;
+	std::vector<int> sol;
+	while (i > 0)
+	{
+		int ir = i - 1;
+		if (m[i] != m[i - 1]) // ese area fue usada
+		{
+			sol.push_back(areas[ir]);
+			i = prev[ir] + 1;
+		}
+		else
+		{
+			--i;
+		}
+	}
+
+	std::reverse(sol.begin(), sol.end());
+	for (const auto s : sol)
+		std::cout << s << ' ';
+	std::cout << '\n';
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	//std::cout << "Pulsa Intro para salir..." << std::flush;
+	//std::cin.get();
+#endif
+	return 0;
+}
