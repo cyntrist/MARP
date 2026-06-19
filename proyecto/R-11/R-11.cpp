ea997c270554d916fb1a7271e05c42b7e4767580
Cynthia Tristán <cyntrist@ucm.es>
2026-06-19 03:23:32 +0200

    R-11

--- proyecto/R-11/R-11.cpp
+++ proyecto/R-11/R-11.cpp
@@ -1,19 +1,101 @@
-// R-11.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
+
+#include <algorithm>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include "Matriz.h"
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+constexpr int INF = 1000000000; // con el max real pasa overflow al hacer temp entonces da la vuelta y el minimo es otro
+
+void Floyd(Matriz<int> const& G, Matriz<int>& C) {
+	int V = G.numfils(); // número de vértices de G
+	// inicialización
+	C = G;
+
+	// actualizaciones de las matrices
+	for (int k = 0; k < V; ++k) {
+		for (int i = 0; i < V; ++i) {
+			for (int j = 0; j < V; ++j) {
+				auto temp = C[i][k] + C[k][j];
+				C[i][j] = std::min(temp, C[i][j]); // es mejor pasar por k
+			}
+		}
+	}
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N = 0;
+	std::cin >> N;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	// resolver el caso posiblemente llamando a otras funciones
+	Matriz G(N, N, INF);
+	for (int i = 0; i < N; ++i)
+		G[i][i] = 0;
+
+	for (int i = 0; i < N - 1; ++i)
+		for (int j = i + 1; j < N; ++j)
+			std::cin >> G[i][j];
+
+	// escribir la solución
+	Matriz<int> C(N, N);
+	Floyd(G, C);
+	for (int i = 0; i < N - 1; ++i)
+	{
+		for (int j = i + 1; j < N; ++j)
+			std::cout << C[i][j] << ' ';
+		std::cout << '\n';
+	}
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	//std::cout << "Pulsa Intro para salir..." << std::flush;
+	//std::cin.get();
+#endif
+	return 0;
+}
