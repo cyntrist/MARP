eb7d77dbac8637d27c70f170efbfd8300b8b50bb
Cynthia Tristán <cyntrist@ucm.es>
2026-06-21 19:44:04 +0200

    r-19

--- proyecto/R-19/R-19.cpp
+++ proyecto/R-19/R-19.cpp
@@ -1,19 +1,101 @@
-// R-19.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
+#include <algorithm>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+//#include "..."  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el costes de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+constexpr int INF = 1000000000; // con el max real pasa overflow al hacer temp entonces da la vuelta y el minimo es otro
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int N = 0;
+	std::cin >> N;
+
+	if (!std::cin) return false;
+
+	// resolver el caso posiblemente llamando a otras funciones
+	int duracion_total = 0;
+	std::vector<int> duraciones(N);
+	for (auto& d : duraciones)
+	{
+		std::cin >> d;
+		duracion_total += d;
+	}
+
+	int mitad = duracion_total * 0.5f;
+	vector m(mitad + 1, false);
+	m[0] = true;
+
+	for (const auto d : duraciones)
+	{
+		for (int j = mitad; j >= d; --j)
+		{
+			if (m[j - d]) m[j] = true;
+		}
+	}
+
+	int mejor = 0;
+	for (int j = mitad; j > 0; --j)
+	{
+		if (m[j])
+		{
+			mejor = j;
+			break;
+		}
+	}
+
+
+	// escribir la solución
+	std::cout << (duracion_total - mejor) << '\n';
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio y parar antes de salir
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+
+	return 0;
+}
