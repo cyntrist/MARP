d0ba24a75b18748da52db974d8a4daf06d8955cb
Cynthia Tristán <cyntrist@ucm.es>
2026-06-21 20:48:41 +0200

    r16 descendente sin optimizar

--- proyecto/R-16/R-16.cpp
+++ proyecto/R-16/R-16.cpp
@@ -1,19 +1,106 @@
-// R-16.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main()
+using namespace std;
+
+#include "Matriz.h"
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool dia_bueno(std::vector<int> const& frutas, int i, int j)
 {
-    std::cout << "Hello World!\n";
+	return frutas[i] != 0 && frutas[i] == frutas[j];
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+int veces_descendente(std::vector<int> const& frutas, int i, int j, Matriz<int>& veces) {
+	if (i >= veces.numfils() || j >= veces.numcols() || i < 0 || j < 0) return 0;
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	int& res = veces[i][j];
+	if (res == -1) {
+		if (i > j) res = 0; // si el trozo es inverso
+		else if (i == j) res = 0; // si el trozo es de tamaño 1
+		else
+		{
+			int o1 = veces_descendente(frutas, i + 2, j, veces) + dia_bueno(frutas, i, i + 1);
+			int o2 = veces_descendente(frutas, i + 1, j - 1, veces) + dia_bueno(frutas, i, j);
+			int o3 = veces_descendente(frutas, i, j - 2, veces) + dia_bueno(frutas, j - 1, j);
+
+			res = std::max(o1, std::max(o2, o3));
+		}
+	}
+	return res;
+}
+
+int veces_ascendente(std::vector<int> const& frutas, int I, int J)
+{
+	Matriz<int> veces(I + 1, J + 1, 0);
+	for (int i = 0; i <= I; ++i)
+	{
+		for (int j = 0; j <= J; ++j)
+		{
+			
+		}
+	}
+	return veces[I][J];
+}
+
+bool resuelveCaso() {
+	int N = 0;
+	std::cin >> N;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	vector<int> frutas(N);
+	for (auto& n : frutas)
+		std::cin >> n;
+
+	Matriz veces(N, N, -1);
+	cout << veces_descendente(frutas, 0, N - 1, veces) << '\n'; 
+	//cout << veces_ascendente(frutas, 0, N - 1) << '\n';
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
