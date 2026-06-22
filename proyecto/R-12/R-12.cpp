a8d720f98daa90a7b1abb09c905d28a58c27290d
Cynthia Tristán <cyntrist@ucm.es>
2026-06-22 03:21:28 +0200

    r12 init

--- proyecto/R-12/R-12.cpp
+++ proyecto/R-12/R-12.cpp
@@ -1,19 +1,87 @@
-// R-12.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
+/*@ <authors>
+ *
+ * Nombre, apellidos y usuario del juez (TAISXXX) de los autores de la solución.
+ *
+ *@ </authors> */
 
 #include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+#include "IndexPQ.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+struct Bateria
 {
-    std::cout << "Hello World!\n";
+	int carga;
+	int tope;
+};
+
+bool resuelveCaso() {
+	int B = 0;
+	std::cin >> B;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	IndexPQ<int, int, std::greater<int>> pq();
+
+	std::vector<int> duraciones(B);
+	for (auto& d : duraciones)
+		std::cin >> d;
+
+	int R = 0;
+	std::cin >> R;
+
+	std::vector<int> repuestos(R);
+	for (auto& r : repuestos)
+		std::cin >> r;
+
+	int Z = 0, T = 0;
+	std::cin >> Z >> T;
+
+	while (T--)
+	{
+		
+	}
+
+	return true;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
